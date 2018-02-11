057a0997a9924e25ad37cb126f506864477a8a5e
ramkumarindia <ramk.india@gmail.com>
2018-02-11 15:42:02 +0530

    test

--- src/test/java/Resources/Run.java
+++ src/test/java/Resources/Run.java
@@ -6,6 +6,10 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 public class Run 
 {
-	
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("dsf");
+
+	}
 
 }
